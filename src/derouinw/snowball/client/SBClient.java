3b4119d7da33dec1e9478d37a0bcc2950e1f4f66
Bill Derouin <derouinbill@gmail.com>
2014-12-14 13:47:34 -0800

    Initial commit

---
+++ src/derouinw/snowball/client/SBClient.java
@@ -0,0 +1,11 @@
+package derouinw.snowball.client;
+
+/**
+ * Main entry point for client
+ */
+public class SBClient {
+
+    public static void main(String[] args) {
+        System.out.println("Client started");
+    }
+}
---
+++ src/derouinw/snowball/server/SBServer.java
@@ -0,0 +1,11 @@
+package derouinw.snowball.server;
+
+/**
+ * Main entry point for server
+ */
+public class SBServer {
+
+    public static void main(String[] args) {
+        System.out.println("Server started");
+    }
+}
